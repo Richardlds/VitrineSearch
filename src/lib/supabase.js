8898e9de7d63b6932ebb3a7f20b224580c5ab42f
Richardlds <117732146+Richardlds@users.noreply.github.com>
2026-09-08 10:35:41 -0300

    feat: implement Supabase client and automate deployment to GitHub Pages

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://yltdjbkntaismaptmaxe.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsdGRqYmtudGFpc21hcHRtYXhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNzg2MzcsImV4cCI6MjEwMzc1NDYzN30.wWSDCuYGx3i2RpPqnyG9ObZWKwch7EYCK6-bkHYDhRA'
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase variables are missing from .env')
+  console.warn('Supabase variables are missing from environment.')
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
